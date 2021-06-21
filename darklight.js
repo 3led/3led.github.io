7cbae4c331dd30eed242870a3e40bb4bfe83caa3
3led <55643100+3led@users.noreply.github.com>
2021-06-20 22:49:46 -0600

    Update darklight.js

--- darklight.js
+++ darklight.js
@@ -1,8 +1,11 @@
+// Computer Science 30 Major Project, Advanced Web Design - Delbert Luo
+// This file allows the user to click a button to toggle on a dark theme
+
+
 var icon = document.getElementById("icon");
 
 icon.onclick = function(){
     document.body.classList.toggle("dark-theme");
-    localStorage.setItem("dark-theme", true)
     if(document.body.classList.contains("dark-theme")){
         icon.src = "https://www.iconpacks.net/icons/2/free-sun-icon-1846-thumb.png"
     }
